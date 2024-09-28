dcd55f3854477f185cbf97aa6d114a54e701bc34
Jung Young Hun <hujung0268@gamil.com>
2024-09-28 12:27:07 +0900

    예제 2-2 Println() 출력문

--- Test/src/section02/ConsolePrint.java
+++ Test/src/section02/ConsolePrint.java
@@ -3,8 +3,8 @@ package section02;
 public class ConsolePrint {
 	public static void main(String[] args) {
 		// 괄호() 안의 데이터를 콘솔창에 출력.
-		System.out.print("Welcome.");
+		System.out.println("Welcome.");
 		// "Welcome." 문자열 옆에 "JAVA World" 문자열 출력.
-		System.out.print("JAVA World");
+		System.out.println("JAVA World");
 	}
 }
