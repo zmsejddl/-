4cea6ef996b04fef7c5055a36ca11c50dcc51712
Jung Young Hun <hujung0268@gamil.com>
2024-09-28 12:37:11 +0900

    예제 2-8 변수 선언 및 초기화

---
+++ Test/src/section02/EX02_08.java
@@ -0,0 +1,9 @@
+package section02;
+
+public class EX02_08 {
+	public static void main(String[] args) {
+		String myCity = "Seoul"; // 도시를 저장할 수 있는 변수를 선언하고.
+									// 변수에 "Seoul"을 대입(저장)했습니다.
+		System.out.println("I am from " + myCity);
+	}
+}
