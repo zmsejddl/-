5598e496e5d78d2a9b04cd10d4134c686afbc23d
Jung Young Hun <hujung0268@gamil.com>
2024-09-28 12:52:36 +0900

    예제 2-15 문자형 1

---
+++ Test/src/section02/EX02_15.java
@@ -0,0 +1,15 @@
+package section02;
+
+public class EX02_15 {
+	public static void main(String[] args) {
+		char ga = '가';
+		char na = '나';
+
+		char alphabetA = 'A';
+		char alphabetB = 'B';
+
+		System.out.println(ga);
+		System.out.println(na);
+		System.out.println(alphabetA + ", " + alphabetB);
+	}
+}
