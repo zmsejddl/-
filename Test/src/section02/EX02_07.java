3bdaedf0be0d4b052ddf8569d533eba6ba89bd89
Jung Young Hun <hujung0268@gamil.com>
2024-09-28 12:35:38 +0900

    예제 2-7 변수에 데이터 입력하기

---
+++ Test/src/section02/EX02_07.java
@@ -0,0 +1,10 @@
+package section02;
+
+public class EX02_07 {
+	public static void main(String[] args) {
+		int studentAge; // 학생의 나이를 저장할 수 있는 변수 선언
+		studentAge = 20; // 변수에 값을 대입(저장)
+
+		System.out.println(studentAge);
+	}
+}
