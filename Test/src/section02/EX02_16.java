50f828a82b5baa318552bc30bab6b559c4fffbb7
Jung Young Hun <hujung0268@gamil.com>
2024-09-28 12:55:01 +0900

    예제 2-16 문자형 2

---
+++ Test/src/section02/EX02_16.java
@@ -0,0 +1,16 @@
+package section02;
+
+public class EX02_16 {
+	public static void main(String[] args) {
+		int ga = '가';
+		int na = '나';
+
+		int alphabetA = 'A'; // 유니코드(정수) 65 - 'A'와 매핑됨
+		int alphabetB = 'B'; // 유니코드(정수) 66 - 'B'와 매핑됨
+
+		System.out.println(ga); // '가'는 44032 유니코드 값 출력
+		System.out.println(na); // '나'는 45208 유니코드 값 출력
+		System.out.println(alphabetA);
+		System.out.println(alphabetB);
+	}
+}
