c8a71137dcb52bf4944c8be81dc31d588b44442d
Jung Young Hun <hujung0268@gamil.com>
2024-09-28 12:44:11 +0900

    예제 2-12 정수형 2

---
+++ Test/src/section02/EX02_12.java
@@ -0,0 +1,15 @@
+package section02;
+
+public class EX02_12 {
+	public static void main(String[] args) {
+		byte num1 = 0110;
+		short num2 = 0107;
+		int num3 = 0x46;
+		long num4 = 69L;
+
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+	}
+}
