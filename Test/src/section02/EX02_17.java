7a0d777af5413c60dd07e9a9327f4c58b08e60e4
Jung Young Hun <hujung0268@gamil.com>
2024-09-28 12:56:50 +0900

    예제 2-17 문자형 3

---
+++ Test/src/section02/EX02_17.java
@@ -0,0 +1,13 @@
+package section02;
+
+public class EX02_17 {
+	public static void main(String[] args) {
+		int alphabetA = 'A'; // 'A'와 매핑된 유니코드(정수) 65가 저장됨
+		int alphabetB = 'B'; // 'B'와 매핑된 유니코드(정수) 66이 저장됨
+		System.out.println(alphabetA);
+		System.out.println(alphabetB);
+
+		char alphabetC = 67; // 유니코드(정수) 67과 매핑된 'C'가 저장됨
+		System.out.println(alphabetC);
+	}
+}
