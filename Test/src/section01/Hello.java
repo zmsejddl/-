b18352a0935ddb6cb2e270b24dfd6c548b0793c7
Jung Young Hun <hujung0268@gamil.com>
2024-09-28 12:22:45 +0900

    주석 작성하기

--- Test/src/section01/Hello.java
+++ Test/src/section01/Hello.java
@@ -1,8 +1,14 @@
 package section01;
 
+/*
+ * 'Hello JAVA!!!' 메시지를 출력하는 프로그램
+ * Date: 2022년 12월 30일
+ * 개발자: 멘토
+ */
 public class Hello {
 
 	public static void main(String[] args) {
+		// 콘솔(화면)에 메시지를 출력함
 		System.out.println("Hello JAVA!!!");
 	}
 
