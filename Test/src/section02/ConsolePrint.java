199f404d1481cd34d5b43c1f5744c3cd23064b1d
Jung Young Hun <hujung0268@gamil.com>
2024-09-28 12:25:33 +0900

    예제 2-1 Print() 출력문

---
+++ Test/src/section02/ConsolePrint.java
@@ -0,0 +1,10 @@
+package section02;
+
+public class ConsolePrint {
+	public static void main(String[] args) {
+		// 괄호() 안의 데이터를 콘솔창에 출력.
+		System.out.print("Welcome.");
+		// "Welcome." 문자열 옆에 "JAVA World" 문자열 출력.
+		System.out.print("JAVA World");
+	}
+}
