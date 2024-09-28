d695549844dca69e9ba6feed551814a6f1b8b3b4
Jung Young Hun <hujung0268@gamil.com>
2024-09-28 12:59:03 +0900

    예제 2-19 논리형 2

---
+++ Test/src/section02/EX02_19.java
@@ -0,0 +1,13 @@
+package section02;
+
+public class EX02_19 {
+	public static void main(String[] args) {
+		boolean isStudent = true;
+
+		if (isStudent) { // isStudent가 true이면,
+			System.out.println("저는 학생입니다.");
+		} else { // isStudent가 false이면,
+			System.out.println("저는 학생이 아닙니다.");
+		}
+	}
+}
