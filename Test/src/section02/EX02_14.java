d4f1dc26d44d7a35a8a9d675ba7bd2b3c417ad1a
Jung Young Hun <hujung0268@gamil.com>
2024-09-28 12:51:11 +0900

    예제 2-14 실수형 2 <정밀도 확인하기>

---
+++ Test/src/section02/EX02_14.java
@@ -0,0 +1,11 @@
+package section02;
+
+public class EX02_14 {
+	public static void main(String[] args) {
+		double pieDouble = 3.141592653589793;
+		float pieFloat = 3.141592653589793f;
+
+		System.out.println("double: " + pieDouble);
+		System.out.println("float: " + pieFloat);
+	}
+}
