8df194c7a13c4dc156d9d4a2862876e515a5cf22
Jung Young Hun <hujung0268@gamil.com>
2024-09-28 12:34:04 +0900

    예제 2-6 소수점 출력하기

---
+++ Test/src/section02/PrintfPt.java
@@ -0,0 +1,15 @@
+package section02;
+
+public class PrintfPt {
+	public static void main(String[] args) {
+		System.out.printf("%.1f", 1.1234567);
+		System.out.println();
+		System.out.printf("%.2f", 1.1234567);
+		System.out.println();
+		System.out.printf("%.3f", 1.1234567);
+		System.out.println();
+		System.out.printf("%.4f", 1.1234567);
+		System.out.println();
+		System.out.printf("%.5f", 1.1234567);
+	}
+}
