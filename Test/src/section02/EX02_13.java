164623a2960bf32f2fc9a3e1e1fe09fa85e78b59
Jung Young Hun <hujung0268@gamil.com>
2024-09-28 12:49:30 +0900

    예제 2-13 실수형 1

---
+++ Test/src/section02/EX02_13.java
@@ -0,0 +1,12 @@
+package section02;
+
+public class EX02_13 {
+	public static void main(String[] args) {
+		double marathon = 42.195;
+		// float halfMarathon=21.0975; 구문 오류 발생
+		float halfMarathon = 21.0975f;
+
+		System.out.println("마라톤은 " + marathon + "km를 달립니다.");
+		System.out.println("하프 마라톤은 " + halfMarathon + "km를 달립니다.");
+	}
+}
