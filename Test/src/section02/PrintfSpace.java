ca83caa3e487a11cdd2cbdc2413a6abc118e2c6c
Jung Young Hun <hujung0268@gamil.com>
2024-09-28 12:32:49 +0900

    예제 2-5 오른쪽 정렬하여 출력하기

---
+++ Test/src/section02/PrintfSpace.java
@@ -0,0 +1,15 @@
+package section02;
+
+public class PrintfSpace {
+	public static void main(String[] args) {
+		System.out.printf("%5d", 1);
+		System.out.println();
+		System.out.printf("%5d", 12);
+		System.out.println();
+		System.out.printf("%5d", 123);
+		System.out.println();
+		System.out.printf("%5d", 1234);
+		System.out.println();
+		System.out.printf("%5d", 12345);
+	}
+}
