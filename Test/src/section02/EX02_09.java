da7cc47d0420d53065599ada4991c17cbfddef4a
Jung Young Hun <hujung0268@gamil.com>
2024-09-28 12:38:17 +0900

    예제 2-9 변수를 이용해 다른 변수에 복사하기

---
+++ Test/src/section02/EX02_09.java
@@ -0,0 +1,11 @@
+package section02;
+
+public class EX02_09 {
+	public static void main(String[] args) {
+		int myAge = 20;
+		int yourAge = myAge;
+
+		System.out.println(myAge);
+		System.out.println(yourAge);
+	}
+}
