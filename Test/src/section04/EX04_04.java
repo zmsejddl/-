3d1863e7751093e4c7748e9a68efc6fcad9c1516
Jung Young Hun <hujung0268@gamil.com>
2024-09-29 12:55:28 +0900

    예제 4-4 if-else 문 2

---
+++ Test/src/section04/EX04_04.java
@@ -0,0 +1,14 @@
+package section04;
+
+public class EX04_04 {
+	public static void main(String[] args) {
+		int a = 4;
+		int b = 10;
+
+		if (a > b) {
+			System.out.println("a가 b보다 큽니다.");
+		} else {
+			System.out.println("a가 b보다 작거나 같습니다.");
+		}
+	}
+}
