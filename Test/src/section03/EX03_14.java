af1a3c3446c40d5ab563dc65c8856b8bfb7cb282
Jung Young Hun <hujung0268@gamil.com>
2024-09-29 12:44:49 +0900

    예제 3-14 관계 연산자 2

---
+++ Test/src/section03/EX03_14.java
@@ -0,0 +1,12 @@
+package section03;
+
+public class EX03_14 {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 11;
+
+		System.out.println(a <= b); // 코드 1
+		System.out.println(a == b); // 코드 2
+		System.out.println(a != --b); // 코드 3
+	}
+}
