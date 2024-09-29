d7c88c74492d8aa926d1fe9a62dad778a0dcb2c3
Jung Young Hun <hujung0268@gamil.com>
2024-09-29 12:50:31 +0900

    예제 3-17 삼항 연산자 2

---
+++ Test/src/section03/EX03_17.java
@@ -0,0 +1,14 @@
+package section03;
+
+public class EX03_17 {
+	public static void main(String[] args) {
+		int num = 0;
+
+		if (7 > 1) {
+			num = 1;
+		} else {
+			num = 2;
+		}
+		System.out.println(num);
+	}
+}
