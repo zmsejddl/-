ba6f2bf8e800f02d3d4f6f27bc509767fec0976b
Jung Young Hun <hujung0268@gamil.com>
2024-09-29 12:54:25 +0900

    예제 4-3 if-else 문 1

---
+++ Test/src/section04/EX04_03.java
@@ -0,0 +1,13 @@
+package section04;
+
+public class EX04_03 {
+	public static void main(String[] args) {
+		int num = 5;
+
+		if (num > 4) {
+			System.out.println(num + "은(는) 4보다 큽니다.");
+		} else {
+			System.out.println(num + "은(는) 4보다 작습니다.");
+		}
+	}
+}
