03c7fec72be4816c1c3efbcc5508f5d888444017
Jung Young Hun <hujung0268@gamil.com>
2024-09-29 12:43:39 +0900

    예제 3-13 관계 연산자 1

---
+++ Test/src/section03/EX03_13.java
@@ -0,0 +1,13 @@
+package section03;
+
+public class EX03_13 {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+
+		System.out.println(a > b);
+		System.out.println(a <= b);
+		System.out.println(a == b);
+		System.out.println(a != b);
+	}
+}
