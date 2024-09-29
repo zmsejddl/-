e0bbbd8c8a0f9d7811baf360b85fc53ed26b01c5
Jung Young Hun <hujung0268@gamil.com>
2024-09-29 12:26:56 +0900

    예제 3-1 부호 연산자

---
+++ Test/src/section03/EX03_01.java
@@ -0,0 +1,12 @@
+package section03;
+
+public class EX03_01 {
+	public static void main(String[] args) {
+		int x = 100;
+		int resultPlus = +x; // + 부호
+		int resultMinus = -x; // - 부호
+
+		System.out.println(resultPlus);
+		System.out.println(resultMinus);
+	}
+}
