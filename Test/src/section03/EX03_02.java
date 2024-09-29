1f417ea9129c8ebbfc2acf5fc8fd4cf375abd70d
Jung Young Hun <hujung0268@gamil.com>
2024-09-29 12:27:54 +0900

    예제 3-2 부호 연산자 <실수>

---
+++ Test/src/section03/EX03_02.java
@@ -0,0 +1,11 @@
+package section03;
+
+public class EX03_02 {
+	public static void main(String[] args) {
+		double d = 1.11;
+		double result = -d;
+
+		System.out.println(-d);
+		System.out.println(result);
+	}
+}
