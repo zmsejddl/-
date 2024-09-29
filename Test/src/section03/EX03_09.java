bf046fa7e61838f850aa17122a3b2b01c27ec3d2
Jung Young Hun <hujung0268@gamil.com>
2024-09-29 12:37:23 +0900

    예제 3-9 산술 연산자 1

---
+++ Test/src/section03/EX03_09.java
@@ -0,0 +1,14 @@
+package section03;
+
+public class EX03_09 {
+	public static void main(String[] args) {
+		int x = 100;
+		int y = 200;
+
+		System.out.println(x + y); // 100 + 200
+		System.out.println(x - y); // 100 - 200
+		System.out.println(x * y); // 100 * 200
+		System.out.println(x / y); // 100 / 200 정수형의 계산으로 0.5가 아닌 0을 반환
+		System.out.println(x % y); // 100 % 200
+	}
+}
