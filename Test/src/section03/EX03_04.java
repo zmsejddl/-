7775b55a702e3425961bb7240eafb5e631313d60
Jung Young Hun <hujung0268@gamil.com>
2024-09-29 12:29:53 +0900

    예제 3-4 증감 연산자 2

---
+++ Test/src/section03/EX03_04.java
@@ -0,0 +1,9 @@
+package section03;
+
+public class EX03_04 {
+	public static void main(String[] args) {
+		int num = 10;
+		System.out.println(num++); // 10이 출력된 후, num = 10 + 1 = 11
+		System.out.println(num);
+	}
+}
