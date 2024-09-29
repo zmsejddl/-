714e87e8b8cef94b5c75db53dadb20b115527b5c
Jung Young Hun <hujung0268@gamil.com>
2024-09-29 12:35:47 +0900

    예제 3-8 논리 부정 연산자

---
+++ Test/src/section03/EX03_08.java
@@ -0,0 +1,9 @@
+package section03;
+
+public class EX03_08 {
+	public static void main(String[] args) {
+		boolean isHuman = false;
+		System.out.println(!isHuman);
+		System.out.println(isHuman);
+	}
+}
