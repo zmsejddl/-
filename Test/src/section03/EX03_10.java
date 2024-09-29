7f51d4ce58e2a327dc7348a0a0e532fa9ba7021a
Jung Young Hun <hujung0268@gamil.com>
2024-09-29 12:38:33 +0900

    예제 3-10 산술 연산자 2 <형 변환>

---
+++ Test/src/section03/EX03_10.java
@@ -0,0 +1,9 @@
+package section03;
+
+public class EX03_10 {
+	public static void main(String[] args) {
+		double num1 = 1.2345;
+		int num2 = 6;
+		System.out.println(num1 + num2); // num2를 double로 형 변환하여 연산
+	}
+}
