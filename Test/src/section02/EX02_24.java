6dd937f73b35cdd64db727cfaaa7dc1cb45090d4
Jung Young Hun <hujung0268@gamil.com>
2024-09-29 12:06:32 +0900

    예제 2-24 실수형<->정수형 형 변환하기

---
+++ Test/src/section02/EX02_24.java
@@ -0,0 +1,22 @@
+package section02;
+
+public class EX02_24 {
+	public static void main(String[] args) {
+		// 실수형 -> 정수형
+		double pie = 3.14;
+		int pieInt = (int) pie;
+		System.out.println(pieInt);
+
+		// 정수형 -> 실수형 (범위 내)
+		int num = 100;
+		double numD = (double) num;
+		float numF = (float) 100;
+		System.out.println(numD);
+		System.out.println(numF);
+
+		// 정수형 -> 실수형 (범위 밖)
+		int i = 99999999;
+		float f = (float) i;
+		System.out.println(f);
+	}
+}
