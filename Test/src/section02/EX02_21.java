60926b25b07a4da793072ec92c5730a618f5294a
Jung Young Hun <hujung0268@gamil.com>
2024-09-29 12:00:18 +0900

    예제 2-21 형 변환하기

---
+++ Test/src/section02/EX02_21.java
@@ -0,0 +1,14 @@
+package section02;
+
+public class EX02_21 {
+	public static void main(String[] args) {
+		int num1 = 11;
+		double num2 = 3.14;
+		double num1Change = (double) num1;
+
+		System.out.println("num1: " + num1);
+		System.out.println("num1Change: " + num1Change);
+		System.out.println(num1Change + num2);
+		System.out.println((int) 1.23);
+	}
+}
