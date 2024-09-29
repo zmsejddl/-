984f2df1a651636a8a5c8c03f195fa15ba2e8509
Jung Young Hun <hujung0268@gamil.com>
2024-09-29 12:01:48 +0900

    예제 2-22 정수형<->정수형 형 변환하기

---
+++ Test/src/section02/EX02_22.java
@@ -0,0 +1,13 @@
+package section02;
+
+public class EX02_22 {
+	public static void main(String[] args) {
+		int i1 = 10;
+		byte b1 = (byte) i1;
+		System.out.println(b1);
+
+		int i2 = 128;
+		byte b2 = (byte) i2;
+		System.out.println(b2);
+	}
+}
