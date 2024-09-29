df2a978c0a445d5e017f16792d4c4dc8a2024747
Jung Young Hun <hujung0268@gamil.com>
2024-09-29 11:58:26 +0900

    예제 2-20 참조 자료형

---
+++ Test/src/section02/EX02_20.java
@@ -0,0 +1,11 @@
+package section02;
+
+public class EX02_20 {
+	public static void main(String[] args) {
+		String hi = "안녕하세요,";
+		String niceToMeetYou = "만나서 반갑습니다.";
+
+		System.out.println(hi);
+		System.out.println(niceToMeetYou);
+	}
+}
