b52fff64bbf07a62366f395fcee09510bd893afd
Jung Young Hun <hujung0268@gamil.com>
2024-09-29 12:51:44 +0900

    예제 4-1 if 문 1

---
+++ Test/src/section04/EX04_01.java
@@ -0,0 +1,11 @@
+package section04;
+
+public class EX04_01 {
+	public static void main(String[] args) {
+		int result = 0;
+		if (3 > 2) {
+			result = 3;
+		}
+		System.out.println(result);
+	}
+}
