9a20cb05d625951c20565fb84c08247378a94656
Jung Young Hun <hujung0268@gamil.com>
2024-09-30 10:49:42 +0900

    예제 4-11 switch 문 2 <break 제외>

---
+++ Test/src/section04/EX04_11.java
@@ -0,0 +1,16 @@
+package section04;
+
+public class EX04_11 {
+	public static void main(String[] args) {
+		int num = 7;
+
+		switch (num) {
+		case 1:
+			System.out.println("num은 1입니다.");
+		case 7:
+			System.out.println("num은 7입니다.");
+		default:
+			System.out.println("num은 1도 7도 아닙니다.");
+		}
+	}
+}
