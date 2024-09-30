75bd0d3eee1739aca3e071efd6452f9991cb3301
Jung Young Hun <hujung0268@gamil.com>
2024-09-30 10:42:30 +0900

    예제 4-7 if-else 문 1

---
+++ Test/src/section04/EX04_07.java
@@ -0,0 +1,15 @@
+package section04;
+
+public class EX04_07 {
+	public static void main(String[] args) {
+		int favorite = 7;
+
+		if (favorite < 5) { // 1번 조건문
+			System.out.println("좋아하는 숫자가 5보다 작습니다."); // 1번 구문
+		} else if (favorite > 5) { // 2번 조건문
+			System.out.println("좋아하는 숫자는 5보다 큽니다."); // 2번 구문
+		} else {
+			System.out.println("좋아하는 숫자가 5입니다.");
+		}
+	}
+}
