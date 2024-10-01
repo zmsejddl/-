d91f202e87c131943a1970deac87e1d9c6441c38
Jung Young Hun <hujung0268@gamil.com>
2024-10-01 12:15:06 +0900

    예제 5-1 for 문 1 <1부터 10까지의 합 출력하기>

---
+++ Test/src/section05/EX05_01.java
@@ -0,0 +1,13 @@
+package section05;
+
+public class EX05_01 {
+	public static void main(String[] args) {
+		int sum = 0;
+
+		for (int i = 1; i <= 10; i++) {
+
+			sum += i;
+		}
+		System.out.println("합: " + sum);
+	}
+}
