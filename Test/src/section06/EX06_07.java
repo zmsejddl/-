6e7ebd0babf2f91fc9f2d08c5eb8b826b76e516a
Jung Young Hun <hujung0268@gamil.com>
2024-10-02 12:37:07 +0900

    예제 6-7 Arrays.ToString()

---
+++ Test/src/section06/EX06_07.java
@@ -0,0 +1,12 @@
+package section06;
+
+import java.util.Arrays;
+
+public class EX06_07 {
+	public static void main(String[] args) {
+
+		int[] arr = { 1, 6, 2, 3, 10, 7, 4, 5, 8, 9 };
+
+		System.out.println(Arrays.toString(arr));
+	}
+}
