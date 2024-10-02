e4a639abf5f2f452f450427209eec2b3bb2d4d88
Jung Young Hun <hujung0268@gamil.com>
2024-10-02 18:01:29 +0900

    예제 7-9 메서드 사용하기

---
+++ Test/src/section07/EX07_09.java
@@ -0,0 +1,10 @@
+package section07;
+
+public class EX07_09 { // 클래스 선언
+	public static void main(String[] args) {
+		Car c = new Car(); // 클래스를 이용해서 객체 생성
+		c.ride(); // 메서드 사용
+		c.ride(); // 메서드 사용
+		c.ride(); // 메서드 사용
+	}
+}
