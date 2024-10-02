fe15853f4219bcb8a204a49587d953b018d3942a
Jung Young Hun <hujung0268@gamil.com>
2024-10-02 17:20:27 +0900

    예제 6-15 2차원 배열을 활용한 예제 1

---
+++ Test/src/section06/EX06_15.java
@@ -0,0 +1,23 @@
+package section06;
+
+public class EX06_15 {
+	public static void main(String[] args) {
+		int[][] arr = new int[5][5];
+
+		int count = 1;
+
+		// 1부터 25까지 차례대로 배열에 값을 넣는다.
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				arr[i][j] = count++;
+			}
+		}
+		// 배열 출력하기
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
