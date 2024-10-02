cdfe9f0eed62ae669ebabf9bdc10c6bbf5f7ba07
Jung Young Hun <hujung0268@gamil.com>
2024-10-02 18:05:37 +0900

    예제 7-12 정적 멤버

---
+++ Test/src/section08/Student.java
@@ -0,0 +1,9 @@
+package section08;
+
+public class Student { // 클래스 선언
+	static String schoolName = "코리아 고등학교"; // 정적 멤버 선언
+
+	static void goToSchool() { // 정적 메서드 선언
+		System.out.println("학교에 갑니다.");
+	}
+}
