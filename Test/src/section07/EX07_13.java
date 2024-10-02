b16e29af1e4d4a6e2accf1d27b6afe84f5942ae4
Jung Young Hun <hujung0268@gamil.com>
2024-10-02 18:07:46 +0900

    예제 7-13 정적 멤버

---
+++ Test/src/section07/EX07_13.java
@@ -0,0 +1,8 @@
+package section07;
+
+public class EX07_13 { // 클래스 선언
+	public static void main(String[] args) {
+		System.out.println(Student.schoolName); // 정적 필드 호출
+		Student.goToSchool(); // 정적 메서드 호출
+	}
+}
