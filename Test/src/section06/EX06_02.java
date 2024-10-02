36b08460cba9ff204c228ed7d04ba89e74a904ff
Jung Young Hun <hujung0268@gamil.com>
2024-10-02 12:21:21 +0900

    예제 6-2 배열에 값 저장하기

---
+++ Test/src/section06/EX06_02.java
@@ -0,0 +1,21 @@
+package section06;
+
+public class EX06_02 {
+	public static void main(String[] args) {
+
+		// 4개의 공간을 가진 배열을 선언
+		int[] arr = new int[4];
+
+		// 배열에 값을 삽입
+		arr[0] = 1;
+		arr[1] = 2;
+		arr[2] = 3;
+		arr[3] = 4;
+
+		// 배열에 값을 출력
+		System.out.println("arr[0] = " + arr[0]);
+		System.out.println("arr[1] = " + arr[1]);
+		System.out.println("arr[2] = " + arr[2]);
+		System.out.println("arr[3] = " + arr[3]);
+	}
+}
