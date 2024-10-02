ab92ec0164489e46dbb0b955be1125d8ccc3fae0
Jung Young Hun <hujung0268@gamil.com>
2024-10-02 18:09:16 +0900

    예제 7-14 인스턴스

--- Test/src/section07/Student.java
+++ Test/src/section07/Student.java
@@ -1,9 +1,14 @@
 package section07;
 
 public class Student { // 클래스 선언
-	static String schoolName = "코리아 고등학교"; // 정적 멤버 선언
+	static String schoolName = "코리아 고등학교"; // 정적 필드 선언
+	String studentName; // 인스턴스 필드 선언
 
 	static void goToSchool() { // 정적 메서드 선언
-		System.out.println("학교에 갑니다.");
+		System.out.println("오늘은 학교에 가는 날입니다.");
+	}
+
+	void hello() { // 인스턴스 메서드 선언
+		System.out.println("안녕하세요, 제 이름은 " + studentName + "입니다.");
 	}
 }
