88a97f15cb002e0723aa65c14fce0001164bcc0b
Jung Young Hun <hujung0268@gamil.com>
2024-10-02 17:58:34 +0900

    예제 7-8 메서드 사용하기

--- Test/src/section07/Car.java
+++ Test/src/section07/Car.java
@@ -1,9 +1,10 @@
 package section07;
 
 public class Car { // 클래스 선언
-	int wheel; // 필드 서언
+	int wheel = 4; // 클래스 필드 선언 및 초기화
 
 	void ride() { // 메서드 선언
 		System.out.println("달립니다.");
+		System.out.println("씽씽씽");
 	}
 }
