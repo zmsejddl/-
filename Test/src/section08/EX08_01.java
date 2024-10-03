060e6564d3bbc58b613baf4a29ebd5f6e95ec248
Jung Young Hun <hujung0268@gamil.com>
2024-10-03 12:18:58 +0900

    예제 8-1 메서드 선언 및 호출

---
+++ Test/src/section08/EX08_01.java
@@ -0,0 +1,13 @@
+package section08;
+
+public class EX08_01 {
+	public static void main(String[] args) {
+		printHello(); // main 메서드 안에서 printHello() 메서드 호출
+	}
+
+	static void printHello() {
+
+		System.out.println("안녕하세요");
+		System.out.println("만나서 반갑습니다.");
+	}
+}
