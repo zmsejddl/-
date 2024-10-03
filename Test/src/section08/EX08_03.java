ee0118943dfdd7358d2d3af353cfba166c5e033a
Jung Young Hun <hujung0268@gamil.com>
2024-10-03 12:21:23 +0900

    예제 8-3 다른 클래스에 있는 메서드 호출하기

---
+++ Test/src/section08/EX08_03.java
@@ -0,0 +1,8 @@
+package section08;
+
+public class EX08_03 {
+	public static void main(String[] args) {
+		Jogger jogger = new Jogger(); // 객체 생성
+		jogger.run(); // jogger 인스턴스의 run() 메서드 호출
+	}
+}
