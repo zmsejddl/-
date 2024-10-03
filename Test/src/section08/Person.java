2ef5a244593ef6b53e386d4ce9d9e77d814de213
Jung Young Hun <hujung0268@gamil.com>
2024-10-03 12:50:08 +0900

    예제 8-20 마음대로 접근 가능한 필드

--- Test/src/section08/Person.java
+++ Test/src/section08/Person.java
@@ -1,11 +1,5 @@
 package section08;
 
 public class Person {
-	void introduce(String name, int age) {
-		System.out.println("제 이름은 " + name + "이고, 나이는 " + age + "세입니다.");
-	}
-
-	void hello() {
-		System.out.println("안녕하세요.");
-	}
+	int age;
 }
