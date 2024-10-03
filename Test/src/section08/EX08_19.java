b4d07ff8291e2ddb9a7cea5850afcc0be6828713
Jung Young Hun <hujung0268@gamil.com>
2024-10-03 12:49:20 +0900

    예제 8-19 return으로 메서드 빠져나가기

---
+++ Test/src/section08/EX08_19.java
@@ -0,0 +1,10 @@
+package section08;
+
+public class EX08_19 {
+	public static void main(String[] args) {
+		int money = 10000;
+
+		Bus bus = new Bus(); // Bus 객체 생성
+		bus.take(money); // bus 인스턴스의 take 메서드 호출
+	}
+}
