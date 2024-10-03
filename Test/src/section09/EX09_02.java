f8a2f2f0e024700e5b77fc45a9e75c7f3aa0dab5
Jung Young Hun <hujung0268@gamil.com>
2024-10-03 16:38:26 +0900

    예제 9-2 생성자

---
+++ Test/src/section09/EX09_02.java
@@ -0,0 +1,9 @@
+package section09;
+
+public class EX09_02 { // 클래스 선언
+	public static void main(String[] args) {
+		Snack chip = new Snack(); // 객체 생성 및 초기화
+		chip.price = 2000; // 객체 필드 설정
+		chip.info(); // 객체 메서드 호출
+	}
+}
