8d32def6527c919147d81c91c8f33d93307e4d37
Jung Young Hun <hujung0268@gamil.com>
2024-10-03 12:51:14 +0900

    에제 8-21 마음대로 수정 가능한 필드

---
+++ Test/src/section08/EX08_21.java
@@ -0,0 +1,9 @@
+package section08;
+
+public class EX08_21 {
+	public static void main(String[] args) {
+		Person kim = new Person();
+		kim.age = -30;
+		System.out.println("kim의 나이는 " + kim.age + "세입니다.");
+	}
+}
