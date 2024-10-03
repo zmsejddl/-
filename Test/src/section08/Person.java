feef203a2a311a34809c134c4d944a0c1b036f8f
Jung Young Hun <hujung0268@gamil.com>
2024-10-03 12:34:10 +0900

    예제 8-10 다른 자료형 2개를 매개변수로 받는 메서드 정의

---
+++ Test/src/section08/Person.java
@@ -0,0 +1,11 @@
+package section08;
+
+public class Person {
+	void introduce(String name, int age) {
+		System.out.println("제 이름은 " + name + "이고, 나이는 " + age + "세입니다.");
+	}
+
+	void hello() {
+		System.out.println("안녕하세요.");
+	}
+}
