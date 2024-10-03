9a9d8577773409b6942cd118c37596962f9e4aea
Jung Young Hun <hujung0268@gamil.com>
2024-10-03 12:38:24 +0900

    예제 8-13 매개변수를 배열로 받는 메서드 호출

---
+++ Test/src/section08/EX08_13.java
@@ -0,0 +1,9 @@
+package section08;
+
+public class EX08_13 {
+	public static void main(String[] args) {
+		int nums[] = { 100, 200 }; // 배열 생성
+		Calc calc = new Calc(); // Calc 객체 생성
+		calc.sum(nums); // calc 인스턴스의 sum 메서드 호출
+	}
+}
