682f9bfd0468af1f17cfcaa59ee039b5de8f7163
Jung Young Hun <hujung0268@gamil.com>
2024-10-03 16:52:20 +0900

    예제 9-9 생성자 추가

--- Test/src/section09/Phone.java
+++ Test/src/section09/Phone.java
@@ -5,7 +5,12 @@ public class Phone { // 클래스 선언
 	int series;
 	String color = "검정색"; // 필드 기본값 초기화
 
-	Phone(String b, int s, String c) {
+	Phone(String b, int s) { // 매개변수를 2개 받는 생성자
+		brand = b;
+		series = s;
+	}
+
+	Phone(String b, int s, String c) { // 매개변수를 3개 받는 생성자
 		brand = b;
 		series = s;
 		color = c;
