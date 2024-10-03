5b5effec39d7431db39501071c7bd41247f241fd
Jung Young Hun <hujung0268@gamil.com>
2024-10-03 12:29:03 +0900

    예제 8-9 매개변수가 2개 이상인 메서드 호출

---
+++ Test/src/section08/EX08_09.java
@@ -0,0 +1,9 @@
+package section08;
+
+public class EX08_09 {
+	public static void main(String[] args) {
+		Calc calc = new Calc(); // 객체 생성
+		calc.sum(5, 3); // calc 인스턴스 sum 메서드 호출
+		calc.sum(10, 7); // calc 인스턴스 sum 메서드 호출
+	}
+}
