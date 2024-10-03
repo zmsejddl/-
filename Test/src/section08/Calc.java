3419c50179240e0a10f297e9aefe541d47146e19
Jung Young Hun <hujung0268@gamil.com>
2024-10-03 12:39:56 +0900

    예제 8-14 result를 반환하는 메서드 정의

--- Test/src/section08/Calc.java
+++ Test/src/section08/Calc.java
@@ -1,13 +1,12 @@
 package section08;
 
 public class Calc {
-	void sum(int[] nums) {
+	public int sum(int[] nums) {
 		int result = 0;
 		for (int i = 0; i < nums.length; i++) {
-			result += nums[i]; // 매개변수로 받은 배열의 요소 값을 하나씩
-								// result 값에 더함
+			result += nums[i];
 		}
 
-		System.out.println("숫자들의 합은 " + result + "입니다.");
+		return result; // 매개변수로 받은 값을 모두 더한 result 값을 다시 반환함
 	}
 }
