f44a4d319f6fe77e58f986634093cf16bf84ddea
Jung Young Hun <hujung0268@gamil.com>
2024-10-03 12:23:01 +0900

    예제 8-4 2개 이상의 메서드 선언하기 1

--- Test/src/section08/Jogger.java
+++ Test/src/section08/Jogger.java
@@ -1,7 +1,14 @@
 package section08;
 
 public class Jogger {
+	String name; // 조거의 이름
+
 	void run() {
 		System.out.println("run run run!");
+	}
+
+	void sayName() {
+		// 필드를 이용해 아래 문장 출력
+		System.out.println("제 이름은 " + name + "입니다.");
 	}
 }
