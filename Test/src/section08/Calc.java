689641ccba53f74b2d5a4fe43a2e027521c6390b
Jung Young Hun <hujung0268@gamil.com>
2024-10-03 12:37:03 +0900

    예제 8-12 매개변수를 배열로 받는 메서드 정의

--- Test/src/section08/Calc.java
+++ Test/src/section08/Calc.java
@@ -1,7 +1,13 @@
 package section08;
 
 public class Calc {
-	void sum(int num1, int num2) {
-		System.out.println("두 수의 합은 " + (num1 + num2) + "입니다.");
+	void sum(int[] nums) {
+		int result = 0;
+		for (int i = 0; i < nums.length; i++) {
+			result += nums[i]; // 매개변수로 받은 배열의 요소 값을 하나씩
+								// result 값에 더함
+		}
+
+		System.out.println("숫자들의 합은 " + result + "입니다.");
 	}
 }
