57887579e950e365add55997599ac4822b125ada
Jung Young Hun <hujung0268@gamil.com>
2024-10-03 12:35:19 +0900

    예제 8-11 다른 자료형 2개를 매개변수로 받는 메서드 호출

---
+++ Test/src/section08/EX08_11.java
@@ -0,0 +1,9 @@
+package section08;
+
+public class EX08_11 {
+	public static void main(String[] args) {
+		Person hong = new Person(); // 객체 생성
+		hong.introduce("홍길동", 20); // hong 인스턴스 introduce 메서드 호출
+		hong.hello();
+	}
+}
