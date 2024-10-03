4c4964058e58ed01c2dc578136ae10accd139577
Jung Young Hun <hujung0268@gamil.com>
2024-10-03 16:45:21 +0900

    예제 9-4 생성자 추가하고 사용하기

---
+++ Test/src/section09/EX09_04.java
@@ -0,0 +1,9 @@
+package section09;
+
+public class EX09_04 { // 클래스 선언
+	public static void main(String[] args) {
+		Snack chip = new Snack(5000); // 객체 생성 및 필드 초기화
+		// chip.price = 2000;
+		chip.info(); // 객체 메서드 호출
+	}
+}
