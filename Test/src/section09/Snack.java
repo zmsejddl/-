89c6eb4ddfd0db244b8b647527d65f51eb7554f7
Jung Young Hun <hujung0268@gamil.com>
2024-10-03 16:39:26 +0900

    예제 9-3 생상자 추가하고 사용하기

--- Test/src/section09/Snack.java
+++ Test/src/section09/Snack.java
@@ -3,6 +3,10 @@ package section09;
 public class Snack { // 클래스 선언
 	int price;
 
+	Snack(int p) { // 직접 선언한 생성자
+		price = p; // 매개변수로 받은 값을 필드 price에 대입
+	}
+
 	void info() {
 		System.out.println("과자의 가격은 " + price + "원입니다.");
 	}
