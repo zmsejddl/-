235069f27c13bbc219a94cc5032610c2d7034385
Jung Young Hun <hujung0268@gamil.com>
2024-10-03 16:51:21 +0900

    예제 9-8 매개변수 부족 컴파일 오류

---
+++ Test/src/section09/EX09_08.java
@@ -0,0 +1,8 @@
+package section09;
+
+public class EX09_08 { // 클래스 선언
+	public static void main(String[] args) {
+		Phone p1 = new Phone("갤럭시", 1, "흰색");
+		Phone p2 = new Phone("아이폰", 2); // 컴파일 오류
+	}
+}
