f337405230028e9ea3f761ca4d97133252170a90
Jung Young Hun <hujung0268@gamil.com>
2024-10-03 16:47:42 +0900

    예제 9-6 매개변수 2개로 객체 생성하기

---
+++ Test/src/section09/EX09_06.java
@@ -0,0 +1,11 @@
+package section09;
+
+public class EX09_06 { // 클래스 선언
+	public static void main(String[] args) {
+		Person p1 = new Person("김자바", 20);
+		Person p2 = new Person("이코딩", 40);
+
+		p1.introduce();
+		p2.introduce();
+	}
+}
