a9126095c6a827061d6e2a00a1c4fb45e53f6d47
Jung Young Hun <hujung0268@gamil.com>
2024-10-04 12:59:57 +0900

    예제 10-25 final 메서드

--- Test/src/section10/Book.java
+++ Test/src/section10/Book.java
@@ -1,10 +1,19 @@
 package section10;
 
-public class Book {
+public class Book { // 클래스 선언
 	String title;
-	int price;
+	String author;
 
-	void info() {
-		System.out.println("책의 제목은 " + title + "이고, 가격은 " + price + "원입니다.");
+	Book(String title, String author) {
+		this.title = title;
+		this.author = author;
+	}
+
+	final void info_title() {
+		System.out.println("책의 제목은 " + title + "입니다.");
+	}
+
+	void info_author() {
+		System.out.println("책의 저자는 " + author + "입니다.");
 	}
 }
