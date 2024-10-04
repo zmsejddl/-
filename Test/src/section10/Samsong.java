96cb640e25003dccacf87b31c74dacba5c72a4f4
Jung Young Hun <hujung0268@gamil.com>
2024-10-04 12:34:53 +0900

    예제 10-10 super

--- Test/src/section10/Samsong.java
+++ Test/src/section10/Samsong.java
@@ -3,6 +3,7 @@ package section10;
 public class Samsong extends Computer { // 클래스 선언
 	@Override
 	void powerOn() {
+		super.powerOn(); // 부모 클래스(Computer)의 powerOn() 메서드 호출
 		System.out.println("아이 러브 삼송");
 	}
 }
