b685ae3c7139ac9cc2676780df48d47a06491915
Jung Young Hun <hujung0268@gamil.com>
2024-10-04 11:53:10 +0900

    예제 10-3 상속

---
+++ Test/src/section10/EX10_03.java
@@ -0,0 +1,10 @@
+package section10;
+
+public class EX10_03 { // 클래스 선언
+	public static void main(String[] args) {
+		Comic comicBook = new Comic();
+		comicBook.title = "포켓몬";
+		comicBook.price = 4500;
+		comicBook.info();
+	}
+}
