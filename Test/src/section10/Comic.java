735ef2605a4b3588610e36ca5be731c30c54a6a1
Jung Young Hun <hujung0268@gamil.com>
2024-10-04 13:05:29 +0900

    예제 10-26 final 메서드

--- Test/src/section10/Comic.java
+++ Test/src/section10/Comic.java
@@ -1,5 +1,29 @@
 package section10;
 
 public class Comic extends Book { // Book 클래스를 상속받는 자식 클래스 Comic
-	// Comic 클래스에서 구현한 것은 아무것도 없음
+	boolean isColor;
+
+	Comic(String title, String author, boolean isColor) {
+		super(title, author);
+		this.isColor = isColor;
+	}
+
+	/*
+	 * @Override void info_title() { // 부모 클래스에서 final로 선언된 메서드르 오버라이딩 시도하면 Cannot
+	 * override the final method from Parent // 이러한 에러가 발생합니다.
+	 * System.out.println("이 만화책의 제목은 " + title + "입니다."); }
+	 */
+
+	@Override
+	void info_author() { // 이 메서드는 Override가 가능합니다.
+		System.out.println("이 만화책의 저자는 " + author + "입니다.");
+	}
+
+	void info_color() {
+		if (isColor) {
+			System.out.println("이 만화책은 컬러입니다.");
+		} else {
+			System.out.println("이 만화책은 흑백입니다.");
+		}
+	}
 }
