2799d7a179e44a3524c5567721e6c22e11875e30
Jung Young Hun <hujung0268@gamil.com>
2024-10-04 12:01:36 +0900

    예제 10-6 상속에서 생성자

---
+++ Test/src/section10/EX10_06.java
@@ -0,0 +1,11 @@
+package section10;
+
+public class EX10_06 { // 클래스 선언
+	public static void main(String[] args) {
+		Customer c1 = new Customer("박자바", 25, 11111);
+		c1.enter();
+
+		Customer c2 = new Customer("송코딩", 20, 22222);
+		c2.enter();
+	}
+}
