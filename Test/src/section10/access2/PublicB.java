fa766b960f642842132c26cd8b5c762880bcff03
Jung Young Hun <hujung0268@gamil.com>
2024-10-04 12:44:00 +0900

    예제 10-15 접근 제한자 default

--- Test/src/section10/access2/PublicB.java
+++ Test/src/section10/access2/PublicB.java
@@ -1,10 +1,11 @@
 package section10.access2;
 
-import section10.access1.PublicA; //다른 패키지의 클래스를 사용하기 위한 import
+import section10.access1.*; // access1 패키지의 모든 클래스를 사용하기 위한 import
 
 public class PublicB { // 클래스 선언
 	public static void main(String[] args) {
-		PublicA a = new PublicA(10);
-		a.printA();
+		DefaultC c = new DefaultC(); // <- 에러: The type PublicA is not visible
+		// c.variableC = 10; <- 필드가 public이더라도 객체를 생성하지 못하기 때문에
+		// 사용할 수 없습니다.
 	}
 }
