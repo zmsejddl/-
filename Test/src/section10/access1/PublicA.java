d9095e6e710f80cf1fffd482c1679f8e382b2cae
Jung Young Hun <hujung0268@gamil.com>
2024-10-04 12:36:44 +0900

    예제 10-11 접근 제한자 public

---
+++ Test/src/section10/access1/PublicA.java
@@ -0,0 +1,13 @@
+package section10.access1;
+
+public class PublicA { // 클래스 선언
+	public int a;
+
+	public PublicA(int a) {
+		this.a = a;
+	}
+
+	public void printA() {
+		System.out.println("PiblicA 클래스의 printA() 메서드입니다.");
+	}
+}
