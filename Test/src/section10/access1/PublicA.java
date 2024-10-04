8ab1b4369fef124f095410df52514e26523d2e3c
Jung Young Hun <hujung0268@gamil.com>
2024-10-04 12:40:44 +0900

    예제 10-14 접근 제한자 default

--- Test/src/section10/access1/PublicA.java
+++ Test/src/section10/access1/PublicA.java
@@ -1,13 +1,9 @@
 package section10.access1;
 
 public class PublicA { // 클래스 선언
-	public int a;
+	DefaultC dc = new DefaultC(); // 같은 패키지이기 때문에 객체 생성 가능
 
-	public PublicA(int a) {
-		this.a = a;
-	}
-
-	public void printA() {
-		System.out.println("PiblicA 클래스의 printA() 메서드입니다.");
+	void methodA() {
+		dc.variableC = 20; // public으로 선언된 필드도 객체를 통해 접근 가능
 	}
 }
