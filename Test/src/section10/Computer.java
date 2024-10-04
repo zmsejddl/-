d91be4825f9b4e7b549601ff098939a55f6fe9c9
Jung Young Hun <hujung0268@gamil.com>
2024-10-04 12:29:14 +0900

    예제 10-7 메서드 오버라이딩

---
+++ Test/src/section10/Computer.java
@@ -0,0 +1,11 @@
+package section10;
+
+public class Computer { // 클래스 선언
+	void powerOn() {
+		System.out.println("삑- 컴퓨터가 켜졌습니다");
+	}
+
+	void powerOff() {
+		System.out.println("컴퓨터가 종료됩니다.");
+	}
+}
