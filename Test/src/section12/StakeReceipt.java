50e833e4cba080a18ce2473a004e123bd168a349
Jung Young Hun <hujung0268@gamil.com>
2024-10-07 11:41:16 +0900

    예제 12-3 추상 클래스

---
+++ Test/src/section12/StakeReceipt.java
@@ -0,0 +1,11 @@
+package section12;
+
+public class StakeReceipt extends Receipt {
+	StakeReceipt(String chef) {
+		super(chef);
+	}
+
+	void trillStake() {
+		System.out.println("스테이크를 앗있게 굽습니다.");
+	}
+}
