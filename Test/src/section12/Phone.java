097e13327b652bcb512e4ac1d1e7c3ec0598d906
Jung Young Hun <hujung0268@gamil.com>
2024-10-07 11:55:49 +0900

    예제 12-10 상수 추가

--- Test/src/section12/Phone.java
+++ Test/src/section12/Phone.java
@@ -1,11 +1,16 @@
 package section12;
 
 public interface Phone {
-	abstract void openingLogo();
+	public static final int MAX_BATTERY_CAPACITY = 100;
 
 	abstract void powerOn(); // <- 공통 구현부가 사라짐
 
 	abstract void powerOff(); // <- 공통 구현부가 사라짐
 
+	abstract boolean isOn(); // 새로운 메서드 추가
+
+	abstract void watchUtube(); // 새로운 메서드 추가
+
 	abstract void charge(); // <- 새로운 메서드 추가
+
 }
