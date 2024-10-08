96ac77ba51860893cd7ad15f4c7081c4d90ac521
Jung Young Hun <hujung0268@gamil.com>
2024-10-08 11:47:34 +0900

    예제 12-11 구현 클래스

--- Test/src/section12/PineapplePhone.java
+++ Test/src/section12/PineapplePhone.java
@@ -1,9 +1,56 @@
 package section12;
 
-public class PineapplePhone extends Phone {
+public class PineapplePhone implements Phone {
+	int batteryCapacity = 40;
+	boolean isOn = false;
+
 	@Override
-	void openingLogo() { // 구현하지 않으면 에러 발생
-		System.out.println("@@@");
+	public void powerOn() {
+		if (batteryCapacity > 30) {
+			System.out.println("@@@Power On!!@@@\n");
+			isOn = true;
+		} else {
+			System.out.println("Low Battery...");
+		}
+	}
+
+	@Override
+	public void powerOff() {
+		System.out.println("@@@Power Off!!@@@\n");
+		isOn = false;
+	}
+
+	@Override
+	public boolean isOn() {
+		if (isOn) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public void watchUtube() {
+		if (batteryCapacity > 10) {
+			System.out.println("--- watching Utube ---");
+			batteryCapacity -= 10;
+			System.out.println("battery is..." + batteryCapacity + "%\n");
+		} else {
+			System.out.println("Low battery...");
+			powerOff();
+		}
+	}
+
+	@Override
+	public void charge() {
+		if (batteryCapacity < Phone.MAX_BATTERY_CAPACITY - 20) {
+			System.out.println("--- charging ---");
+			batteryCapacity += 5;
+			System.out.println("Charged..." + batteryCapacity + "%\n");
+		} else {
+			System.out.println("You don't have to charge...");
+			System.out.println("It's enough..." + batteryCapacity + "%");
+		}
 	}
 
 }
