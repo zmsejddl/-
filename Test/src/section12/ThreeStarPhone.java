ad6706909f7e05b176a821de5a514b8b88b1fac4
Jung Young Hun <hujung0268@gamil.com>
2024-10-08 11:51:12 +0900

    예제 12-12 다형성

--- Test/src/section12/ThreeStarPhone.java
+++ Test/src/section12/ThreeStarPhone.java
@@ -1,9 +1,55 @@
 package section12;
 
-public class ThreeStarPhone extends Phone {
+public class ThreeStarPhone implements Phone {
+	int batteryCapacity = 35;
+	boolean isOn = false;
+
 	@Override
-	void openingLogo() { // 구현하지 않으면 에러 발생
-		System.out.println("***");
+	public void powerOn() {
+		if (batteryCapacity > 30) {
+			System.out.println("***폰이 켜졌습니다.***\n");
+			isOn = true;
+		} else {
+			System.out.println("배터리가 부족합니다...");
+		}
 	}
 
+	@Override
+	public void powerOff() {
+		System.out.println("***폰이 꺼졌습니다.***\n");
+		isOn = false;
+	}
+
+	@Override
+	public boolean isOn() {
+		if (isOn) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public void watchUtube() {
+		if (batteryCapacity > 15) {
+			System.out.println("--- U튜브 시청 중 ---");
+			batteryCapacity -= 10;
+			System.out.println("잔여 배터리..." + batteryCapacity + "%\n");
+		} else {
+			System.out.println("배터리가 부족합니다...");
+			powerOff();
+		}
+	}
+
+	@Override
+	public void charge() {
+		if (batteryCapacity < Phone.MAX_BATTERY_CAPACITY - 20) {
+			System.out.println("--- 충전 중 ---");
+			batteryCapacity += 10;
+			System.out.println("잔여 배터리..." + batteryCapacity + "%\n");
+		} else {
+			System.out.println("충전할 필요가 없습니다.");
+			System.out.println("잔여 배터리..." + batteryCapacity + "%");
+		}
+	}
 }
