111538cb9ee3ecb0dbb34cd0bfb9494e534ca326
Jung Young Hun <hujung0268@gamil.com>
2024-10-10 11:57:02 +0900

    예제 14-6 예외 처리가 한 개인 경우

---
+++ Test/src/section14/EX14_06.java
@@ -0,0 +1,24 @@
+package section14;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class EX14_06 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		try {
+			int[] cards = { 4, 5, 1, 2, 7, 8 };
+			System.out.println("몇 번째 카드를 뽑으시겠습니까? >>");
+
+			int cardIndex = scan.nextInt();
+			System.out.println("뽑은 카드 번호는: " + cards[cardIndex]);
+
+		} catch (InputMismatchException e) {
+			System.out.println("잘못 입력하셨습니다. 숫자만 가능합니다.");
+		}
+
+		System.out.println("프로그램 종료");
+		scan.close();
+	}
+}
