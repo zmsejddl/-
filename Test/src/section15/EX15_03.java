c1f6dbf6ebfc8c2dc7ff4dcd1f38c8a30996789f
Jung Young Hun <hujung0268@gamil.com>
2024-10-11 11:47:10 +0900

    예제 15-3 hashCode() 객체 비교

---
+++ Test/src/section15/EX15_03.java
@@ -0,0 +1,13 @@
+package section15;
+
+public class EX15_03 {
+	public static void main(String[] args) {
+		String str1 = "hello";
+		String str2 = "hello";
+		String str3 = "apple";
+
+		System.out.println("str1 hashCode: " + str1.hashCode());
+		System.out.println("str2 hashCode: " + str2.hashCode());
+		System.out.println("str3 hashCode: " + str3.hashCode());
+	}
+}
