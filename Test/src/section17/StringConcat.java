3403eeef7c87cbf7b8efa67c185afa5e73ba5730
Jung Young Hun <hujung0268@gamil.com>
2024-10-14 11:37:17 +0900

    예제 17-4 지역변수 str 사용

--- Test/src/section17/StringConcat.java
+++ Test/src/section17/StringConcat.java
@@ -1,18 +1,16 @@
 package section17;
 
-//두 문장을 결합하는 함수 생성
 interface StrConcat {
 	String concat(String str1, String str2);
 }
 
 public class StringConcat {
 	public static void main(String[] args) {
-		String str = "";
-
+		final String str = "java";
 		StrConcat strConcat = (s, v) -> {
-			// 지역변수 재정의 시 오류
-			str = s + v;
-			return str;
+			String sentence = str + "! " + s + " " + v;
+			return sentence;
 		};
+		System.out.println(strConcat.concat("Hello", "world"));
 	}
 }
