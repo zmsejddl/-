dc8dac16aecec89754c812c25f37d62140122de1
Jung Young Hun <hujung0268@gamil.com>
2024-10-15 12:10:34 +0900

    예제 18-4 Runnable을 상속하는 Thread

--- Test/src/section18/RunnableExample.java
+++ Test/src/section18/RunnableExample.java
@@ -1,19 +1,15 @@
 package section18;
 
-//Runnable을 상속
-class whiteFlag implements Runnable {
-	@Override
-	public void run() {
-		while (true) {
-			System.out.println("백기 올려");
-		}
-	}
-}
-
-//스레드 메인
 public class RunnableExample {
 	public static void main(String[] args) {
-		Thread write = new Thread(new whiteFlag());
-		write.start();
+		// Runnable 인터페이스를 익명 객체로 처리
+		Runnable white = () -> {
+			while (true) {
+				System.out.println("백기 올려");
+			}
+		};
+
+		Thread whiteFlag = new Thread(white);
+		whiteFlag.start();
 	}
 }
