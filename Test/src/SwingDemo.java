70d4f7a853e3fe9934bdd0ee4826acbe1deed2d0
Jung Young Hun <hujung0268@gamil.com>
2024-10-20 11:57:20 +0900

    JTextField

--- Test/src/SwingDemo.java
+++ Test/src/SwingDemo.java
@@ -1,16 +1,23 @@
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
-//JFrame을 상속받아서 SwingDemo로 창을 구현합니다.
 public class SwingDemo extends JFrame {
 
-	SwingDemo() { // 생성자
-		// 창 크기(가로, 세로) 설정
-		setSize(300, 200);
+	JTextField tf_num1 = new JTextField(); // 텍스트 입력 칸 생성
+	JTextField tf_num2 = new JTextField();
 
-		// 프레임을 닫았을 때 프로세스까지 제거되도록 설정
+	SwingDemo() { // 생성자
+		setSize(300, 200);
+		setLayout(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		// 프레임이 화면에 보이도록 설정
+		tf_num1.setBounds(50, 50, 40, 40); // 컴포넌트 위치, 크기 설정
+		// setBounds(창의 왼쪽에서 50, 위쪽에서 50, 필드 폭 40, 높이 40)
+		tf_num2.setBounds(100, 50, 40, 40);
+
+		add(tf_num1);
+		add(tf_num2);
+
 		setVisible(true);
 	}
 
