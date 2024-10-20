10028967e7cf4a0867923a0b796b56a77be56f0a
Jung Young Hun <hujung0268@gamil.com>
2024-10-20 11:59:59 +0900

    JButton

--- Test/src/SwingDemo.java
+++ Test/src/SwingDemo.java
@@ -1,22 +1,31 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 public class SwingDemo extends JFrame {
 
-	JTextField tf_num1 = new JTextField(); // 텍스트 입력 칸 생성
+	JTextField tf_num1 = new JTextField();
 	JTextField tf_num2 = new JTextField();
+
+	JButton btn_plus = new JButton("더하기");
+
+	JLabel la_result = new JLabel("결과 출력");
 
 	SwingDemo() { // 생성자
 		setSize(300, 200);
 		setLayout(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		tf_num1.setBounds(50, 50, 40, 40); // 컴포넌트 위치, 크기 설정
-		// setBounds(창의 왼쪽에서 50, 위쪽에서 50, 필드 폭 40, 높이 40)
+		tf_num1.setBounds(50, 50, 40, 40);
 		tf_num2.setBounds(100, 50, 40, 40);
+		btn_plus.setBounds(150, 50, 80, 40);
+		la_result.setBounds(50, 100, 200, 40);
 
 		add(tf_num1);
 		add(tf_num2);
+		add(btn_plus);
+		add(la_result);
 
 		setVisible(true);
 	}
