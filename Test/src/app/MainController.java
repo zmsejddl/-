2d3fa90e7cfe1679fdc19a667e610373c5ec97cb
Jung Young Hun <hujung0268@gamil.com>
2024-10-21 10:26:09 +0900

    Main 패키지 구현

--- Test/src/app/MainController.java
+++ Test/src/app/MainController.java
@@ -1,5 +1,33 @@
 package app;
 
-public class MainController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import game.GameController;
+import rank.RankController;
+
+public class MainController implements ActionListener {
+	MainView mainView;
+
+	public MainController() {
+		mainView = new MainView();
+		mainView.btn_game.addActionListener(this); // 각 버튼에 귀를 달아줍니다.
+		mainView.btn_rank.addActionListener(this);
+	}
+
+	// ActionListener 인터페이스의 추상 메서드
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == mainView.btn_game) { // 게임 시작 버튼이 클릭되면
+			new GameController();
+			mainView.setVisible(false); // 메인 화면이 사라집니다.
+		} else if (e.getSource() == mainView.btn_rank) { // 랭킹 확인 버튼이 클릭되면
+			new RankController();
+			mainView.setVisible(false);
+		}
+	}
+
+	public static void main(String[] args) {
+		new MainController(); // 프로그램의 시작을 담당합니다.
+	}
 }
--- Test/src/app/MainView.java
+++ Test/src/app/MainView.java
@@ -1,5 +1,22 @@
 package app;
 
-public class MainView {
+import java.awt.FlowLayout;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class MainView extends JFrame {
+	JButton btn_game = new JButton("게임 시작");
+	JButton btn_rank = new JButton("랭킹 확인");
+
+	public MainView() {
+		// 레이아웃 설정
+		setLayout(new FlowLayout());
+
+		add(btn_game);
+		add(btn_rank);
+		setSize(600, 300);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
