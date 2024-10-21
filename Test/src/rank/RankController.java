27a8d0e14f985e5bcf0f3ed387849d258c84ffff
Jung Young Hun <hujung0268@gamil.com>
2024-10-21 10:56:56 +0900

    Rank 패키지 구현

--- Test/src/rank/RankController.java
+++ Test/src/rank/RankController.java
@@ -1,5 +1,28 @@
 package rank;
 
-public class RankController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import app.MainController;
+import util.DBcon;
+
+public class RankController implements ActionListener {
+
+	RankView rankView;
+
+	public RankController() {
+		DBcon db = new DBcon();
+		String[][] resultArr = db.findAll();
+		rankView = new RankView(resultArr);
+		rankView.btn_exit.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == rankView.btn_exit) {
+			new MainController();
+			rankView.setVisible(false);
+		}
+	}
 
 }
--- Test/src/rank/RankView.java
+++ Test/src/rank/RankView.java
@@ -1,5 +1,37 @@
 package rank;
 
-public class RankView {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
+public class RankView extends JFrame {
+	JLabel la_rank = new JLabel("<랭킹 안내>");
+
+	JButton btn_exit = new JButton("메인으로");
+
+	public RankView(String[][] arr) {
+		setTitle("랭킹");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		setLayout(null);
+
+		String rank[][] = arr;
+		String column[] = { "순위", "이름", "걸린 시간" };
+
+		JTable ta_rank = new JTable(rank, column);
+		JScrollPane scrollpane = new JScrollPane(ta_rank);
+
+		la_rank.setBounds(200, 30, 100, 30);
+		btn_exit.setBounds(470, 10, 80, 30);
+		scrollpane.setBounds(160, 60, 200, 200);
+
+		add(la_rank);
+		add(scrollpane);
+		add(btn_exit);
+
+		setSize(600, 400);
+		setVisible(true);
+	}
 }
